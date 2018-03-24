f0f848b32cd8be41a1399c8b075c20f4b4363833
Andrew Warren <aswarren@gmail.com>
2018-03-24 16:50:29 -0400

    contrast enhancements

--- prok_tuxedo.py
+++ prok_tuxedo.py
@@ -9,8 +9,8 @@ import tarfile
 
 #pretty simple: its for prokaryotes in that parameters will be attuned to give best performance and no tophat
 
-def run_alignment(genome_list, library_dict, parameters, output_dir): 
-    #modifies library_dict sub replicates to include 'bowtie' dict recording output files
+def run_alignment(genome_list, condition_dict, parameters, output_dir): 
+    #modifies condition_dict sub replicates to include 'bowtie' dict recording output files
     for genome in genome_list:
         genome_link=os.path.join(output_dir, os.path.basename(genome["genome"]))
         final_cleanup=[]
@@ -34,9 +34,9 @@ def run_alignment(genome_list, library_dict, parameters, output_dir):
             genome["dir"]=genome["dir"][:-1]
         genome["dir"]=os.path.abspath(genome["dir"])
         genome["output"]=os.path.join(output_dir,os.path.basename(genome["dir"]))
-        for library in library_dict:
+        for library in condition_dict:
             rcount=0
-            for r in library_dict[library]["replicates"]:
+            for r in condition_dict[library]["replicates"]:
                 cur_cleanup=[]
                 rcount+=1
                 target_dir=os.path.join(genome["output"],library,"replicate"+str(rcount))
@@ -72,7 +72,7 @@ def run_alignment(genome_list, library_dict, parameters, output_dir):
         for garbage in final_cleanup:
             subprocess.call(["rm", garbage])
 
-def run_cufflinks(genome_list, library_dict, parameters, output_dir):
+def run_cufflinks(genome_list, condition_dict, parameters, output_dir):
     for genome in genome_list:
         genome_file=genome["genome"]
         genome_link=os.path.join(output_dir, os.path.basename(genome["genome"]))
@@ -81,8 +81,8 @@ def run_cufflinks(genome_list, library_dict, parameters, output_dir):
         cmd=["cufflinks","-q","-g",genome["annotation"],"-b",genome_link,"-I","50"]
         thread_count=multiprocessing.cpu_count()
         cmd+=["-p",str(thread_count)]
-        for library in library_dict:
-            for r in library_dict[library]["replicates"]:
+        for library in condition_dict:
+            for r in condition_dict[library]["replicates"]:
                 cur_dir=os.path.dirname(os.path.realpath(r[genome_file]["bam"]))
                 os.chdir(cur_dir)
                 cur_cmd=list(cmd)
@@ -95,7 +95,7 @@ def run_cufflinks(genome_list, library_dict, parameters, output_dir):
                 else:
                     sys.stderr.write(cuff_gtf+" cufflinks file already exists. skipping\n")
 
-def run_diffexp(genome_list, library_dict, parameters, output_dir, gene_matrix):
+def run_diffexp(genome_list, condition_dict, parameters, output_dir, gene_matrix, contrasts, job_data):
     #run cuffquant on every replicate, cuffmerge on all resulting gtf, and cuffdiff on the results. all per genome.
     for genome in genome_list:
         genome_file=genome["genome"]
@@ -108,19 +108,19 @@ def run_diffexp(genome_list, library_dict, parameters, output_dir, gene_matrix):
         merge_file=os.path.join(merge_folder,"merged.gtf")
         thread_count=multiprocessing.cpu_count()
         merge_cmd+=["-p",str(thread_count),"-o", merge_folder]
-        for library in library_dict:
-            for r in library_dict[library]["replicates"]:
+        for library in condition_dict:
+            for r in condition_dict[library]["replicates"]:
                 with open(merge_manifest, "a") as manifest: manifest.write("\n"+os.path.join(r[genome_file]["dir"],"transcripts.gtf"))
         merge_cmd+=[merge_manifest]
-        diff_cmd=["cuffdiff",merge_file,"-p",str(thread_count),"-b",genome_link,"-L",",".join(library_dict.keys())]
+        diff_cmd=["cuffdiff",merge_file,"-p",str(thread_count),"-b",genome_link,"-L",",".join(condition_dict.keys())]
         if not os.path.exists(merge_file):
             print " ".join(merge_cmd)
             subprocess.check_call(merge_cmd)
         else:
             sys.stderr.write(merge_file+" cuffmerge file already exists. skipping\n")
-        for library in library_dict:
+        for library in condition_dict:
             quant_list=[]
-            for r in library_dict[library]["replicates"]:
+            for r in condition_dict[library]["replicates"]:
                 #quant_cmd=["cuffquant",genome["annotation"],r[genome_file]["bam"]]
                 quant_cmd=["cuffquant",merge_file,r[genome_file]["bam"]]
                 cur_dir=r[genome_file]["dir"]#directory for this replicate/genome
@@ -136,6 +136,12 @@ def run_diffexp(genome_list, library_dict, parameters, output_dir, gene_matrix):
         cur_dir=genome["output"]
         os.chdir(cur_dir)
         cds_tracking=os.path.join(cur_dir,"cds.fpkm_tracking")
+        contrasts_file = os.path.join(cur_dir, "contrasts.txt")
+        with open(contrasts_file,'w') as contrasts_handle:
+            constrasts_handle.write("condition_A\tcondition_B\n")
+            for c in contrasts:
+                contrasts_handle.write(str(c[0])+"\t"+str(c[1])+"\n")
+        diff_cmd+=["--contrast-file",contrasts_file]
         if not os.path.exists(cds_tracking):
             print " ".join(diff_cmd)
             subprocess.check_call(diff_cmd)
@@ -145,12 +151,22 @@ def run_diffexp(genome_list, library_dict, parameters, output_dir, gene_matrix):
             de_file=os.path.join(cur_dir,"gene_exp.diff")
             gmx_file=os.path.join(cur_dir,"gene_exp.gmx")
             cuffdiff_to_genematrix.main([de_file],gmx_file)
-            #convert_cmd=[os.path.join(os.path.realpath(__file__),"p3diffexp","expression_transform.py")]
+            trasform_script =os.path.join(os.path.realpath(__file__),"p3diffexp","expression_transform.py")
+            if os.path.exists(transform_script) and os.path.exists(gmx_file):
+                transform_params = {"output_path":cur_dir, "xfile":gmx_file, "xformat":"tsv",\
+                        "xsetup":"gene_matrix", "source_id_type":"feature_id",\
+                        "data_type":"Transcriptomics", "title":"RNA-Seq", "description":"RNA-Seq",\
+                        "organism":job_data.genome_id}
+                params_file=os.path.join(cur_dir, "diff_exp_params.json")
+                with open(params_file, 'w') as params_handle:
+                    params_handle.write(json.dumps(transform_params))
+                convert_cmd=[transform_script, "--ufile", params_file, "sstring", job_data.sstring]
+                print " ".join(convert_cmd)
             #convert_cmd+=]
             #subprocess.check_call(convert_cmd)
             
 
-def main(genome_list, library_dict, parameters_file, output_dir, gene_matrix=False):
+def main(genome_list, condition_dict, parameters_file, output_dir, gene_matrix=False, contrasts=[], job_data):
     #arguments:
     #list of genomes [{"genome":somefile,"annotation":somefile}]
     #dictionary of library dictionaries structured as {libraryname:{library:libraryname, replicates:[{read1:read1file, read2:read2file}]}}
@@ -161,57 +177,69 @@ def main(genome_list, library_dict, parameters_file, output_dir, gene_matrix=Fal
     	parameters=json.load(open(parameters_file,'r'))
     else:
         parameters=[]
-    run_alignment(genome_list, library_dict, parameters, output_dir)
-    run_cufflinks(genome_list, library_dict, parameters, output_dir)
-    if len(library_dict.keys()) > 1:
-        run_diffexp(genome_list, library_dict, parameters, output_dir, gene_matrix)
+    run_alignment(genome_list, condition_dict, parameters, output_dir)
+    run_cufflinks(genome_list, condition_dict, parameters, output_dir)
+    if len(condition_dict.keys()) > 1:
+        run_diffexp(genome_list, condition_dict, parameters, output_dir, gene_matrix, contrasts, job_data)
 
 
 if __name__ == "__main__":
     #modelling input parameters after rockhopper
     parser = argparse.ArgumentParser()
+    parser.add_argument('--jfile',
+            help='json file for job {"reference_genome_id": "1310806.3", "experimental_conditions":\
+                    ["c1_control", "c2_treatment"], "output_file": "rnaseq_baumanii_1505311", \
+                    "recipe": "RNA-Rocket", "output_path": "/anwarren@patricbrc.org/home/test",\
+                    "paired_end_libs": [{"read1": "/anwarren@patricbrc.org/home/rnaseq_test/MHB_R1.fq.gz",\
+                    "read2": "/anwarren@patricbrc.org/home/rnaseq_test/MHB_R2.fq.gz", "condition": 1},\
+                    {"read1": "/anwarren@patricbrc.org/home/rnaseq_test/MERO_75_R1.fq.gz",\
+                    "read2": "/anwarren@patricbrc.org/home/rnaseq_test/MERO_75_R2.fq.gz", "condition": 2}], "contrasts": [[1, 2]]}', required=True)
+    parser.add_argument('--sstring', help='json server string specifying api {"data_api":"url"}', required=False, default=None)
     parser.add_argument('-g', help='csv list of directories each containing a genome file *.fna and annotation *.gff', required=True)
     parser.add_argument('--index', help='flag for enabling using HISAT2 indices', action='store_true', required=False)
-    parser.add_argument('-L', help='csv list of library names for comparison', required=False)
-    parser.add_argument('-C', help='csv list of comparisons. comparisons are library names separated by percent. ', required=False)
+    #parser.add_argument('-L', help='csv list of library names for comparison', required=False)
+    #parser.add_argument('-C', help='csv list of comparisons. comparisons are library names separated by percent. ', required=False)
     parser.add_argument('-p', help='JSON formatted parameter list for tuxedo suite keyed to program', required=False)
     parser.add_argument('-o', help='output directory. defaults to current directory.', required=False)
     #parser.add_argument('-x', action="store_true", help='run the gene matrix conversion and create a patric expression object', required=False)
-    parser.add_argument('readfiles', nargs='+', help="whitespace sep list of read files. shoudld be \
-            in corresponding order as library list. ws separates libraries,\
-            a comma separates replicates, and a percent separates pairs.")
+    #parser.add_argument('readfiles', nargs='+', help="whitespace sep list of read files. shoudld be \
+    #        in corresponding order as library list. ws separates libraries,\
+    #        a comma separates replicates, and a percent separates pairs.")
     if len(sys.argv) ==1:
         parser.print_help()
         sys.exit(2)
-    args = parser.parse_args()
-    library_dict={}
-    library_list=[]
+    map_args = parser.parse_args()
+    condition_dict={}
+    condition_list=[]
     comparison_list=[]
-    if args.L:
-        library_list=args.L.strip().split(',')
-    if args.C:
-        comparison_list=[i.split("%") for i in args.C.strip().split(',')]
+    #if map_args.L:
+    #    condition_list=map_args.L.strip().split(',')
+    #if args.C:
+    #    comparison_list=[i.split("%") for i in args.C.strip().split(',')]
         
     #create library dict
-    if not len(library_list): library_list.append("results")
+    with open(args.jfile, 'r') as job_handle:
+        job_data = json.load(args.jfile)
+    condition_list= job_data.get("experimental_conditions",[])
+    if not len(condition_list): condition_list.append("results")
     gene_matrix=True
     if not args.o:
         output_dir="./"
     else:
         output_dir=args.o
-    for lib in library_list:
-        library_dict[lib]={"library":lib}
+    for cond in condition_list:
+        condition_dict[cond]={"condition":cond}
     count=0
     #add read/replicate structure to library dict
-    for read in args.readfiles:
-        replicates=read.split(',')
-        rep_store=library_dict[library_list[count]]["replicates"]=[]
-        for rep in replicates:
-            pair=rep.split('%')
-            pair_dict={"read1":pair[0]}
-            if len(pair) == 2:
-                pair_dict["read2"]=pair[1]
-            rep_store.append(pair_dict)
+    replicates={}
+    contrasts=[]
+    for c in job_data.get("contrasts",[]):
+        contrasts.append([condition_list[c[0]-1],condition_list[c[1]-1]])
+    for read in job_data.get("paired_end_libs",[])+job_data.get("single_end_libs",[]):
+        if "read" in read:
+            read["read1"] = read.pop("read")
+        condition_index = int(read.get("condition", count+1))-1 #if no condition return position so everything is diff condition
+        rep_store=condition_dict[condition_list[condition_index]].setdefault("replicates",[]).append(read)
         count+=1
     genome_dirs=args.g.strip().split(',')
     genome_list=[]
@@ -245,6 +273,6 @@ if __name__ == "__main__":
 
         genome_list.append(cur_genome)
     
-    main(genome_list,library_dict,args.p,output_dir,gene_matrix)
+    main(genome_list,condition_dict,args.p,output_dir,gene_matrix,contrasts, map_args)
 
 
