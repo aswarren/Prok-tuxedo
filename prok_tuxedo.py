ce4bf82a04f08cc622ae0097ce4508cebfdf7d40
Andrew Warren <aswarren@gmail.com>
2018-03-28 13:33:28 -0400

    diffexp object param creation

--- prok_tuxedo.py
+++ prok_tuxedo.py
@@ -171,13 +171,14 @@ def run_diffexp(genome_list, condition_dict, parameters, output_dir, gene_matrix
             cuffdiff_to_genematrix.main([de_file],gmx_file)
             transform_script = "expression_transform.py"
             if os.path.exists(gmx_file):
-                transform_params = {"output_path":map_args.d, "xfile":gmx_file, "xformat":"tsv",\
+                experiment_path=os.path.join(map_args.o, map_args.d)
+                subprocess.call(["mkdir","-p",experiment_path])
+                transform_params = {"output_path":experiment_path, "xfile":gmx_file, "xformat":"tsv",\
                         "xsetup":"gene_matrix", "source_id_type":"patric_id",\
                         "data_type":"Transcriptomics", "experiment_title":"RNA-Seq", "experiment_description":"RNA-Seq",\
                         "organism":job_data.get("genome_id")}
                 diffexp_json["parameters"]=transform_params
                 params_file=os.path.join(cur_dir, "diff_exp_params.json")
-                experiment_path=map_args.d
                 with open(params_file, 'w') as params_handle:
                     params_handle.write(json.dumps(transform_params))
                 convert_cmd=[transform_script, "--ufile", params_file, "--sstring", map_args.sstring, "--output_path",experiment_path,"--xfile",gmx_file]
@@ -225,7 +226,7 @@ if __name__ == "__main__":
     #parser.add_argument('-C', help='csv list of comparisons. comparisons are library names separated by percent. ', required=False)
     parser.add_argument('-p', help='JSON formatted parameter list for tuxedo suite keyed to program', required=False)
     parser.add_argument('-o', help='output directory. defaults to current directory.', required=False)
-    parser.add_argument('-d', help='output path / name of file for differential expression job folder where files go', required=False)
+    parser.add_argument('-d', help='name of the folder for differential expression job folder where files go', required=True)
     #parser.add_argument('-x', action="store_true", help='run the gene matrix conversion and create a patric expression object', required=False)
     #parser.add_argument('readfiles', nargs='+', help="whitespace sep list of read files. shoudld be \
     #        in corresponding order as library list. ws separates libraries,\
@@ -299,7 +300,7 @@ if __name__ == "__main__":
         genome_list.append(cur_genome)
 
     #job template for differential expression object
-    json_template = json.loads("""                    {
+    diffexp_json = json.loads("""                    {
                         "app": {
                             "description": "Parses and transforms users differential expression data",
                             "id": "DifferentialExpression",
