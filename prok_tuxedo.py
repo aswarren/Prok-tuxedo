af7dd957ba63acd877904fb6897fb859163181fe
Andrew Warren <aswarren@gmail.com>
2018-09-13 16:57:45 -0400

    fixing bug

--- prok_tuxedo.py
+++ prok_tuxedo.py
@@ -63,49 +63,50 @@ def run_alignment(genome_list, condition_dict, parameters, output_dir, job_data)
         thread_count=multiprocessing.cpu_count()
         cmd+=["-p",str(thread_count)]
         make_directory_names(genome, condition_dict)
-        rcount=0
-        for r in condition_dict[library]["replicates"]:
-            cur_cleanup=[]
-            rcount+=1
-            target_dir=r["target_dir"]
-            fastqc_cmd=["fastqc","--outdir",target_dir]
-            samstat_cmd=["samstat"]
-            subprocess.call(["mkdir","-p",target_dir])
-            cur_cmd=list(cmd)
-            if "read2" in r:
-                cur_cmd+=["-1",link_space(r["read1"])," -2",link_space(r["read2"])]
-                name1=os.path.splitext(os.path.basename(r["read1"]))[0].replace(" ","")
-                name2=os.path.splitext(os.path.basename(r["read2"]))[0].replace(" ","")
-                sam_file=os.path.join(target_dir,name1+"_"+name2+".sam")
-                fastqc_cmd+=[r["read1"],r["read2"]]
-            else:
-                cur_cmd+=[" -U",link_space(r["read1"])]
-                name1=os.path.splitext(os.path.basename(r["read1"]))[0].replace(" ","")
-                sam_file=os.path.join(target_dir,name1+".sam")
-                fastqc_cmd+=[r["read1"]]
-            cur_cleanup.append(sam_file)
-            bam_file=sam_file[:-4]+".bam"
-            samstat_cmd.append(bam_file)
-            r[genome["genome"]]={}
-            r[genome["genome"]]["bam"]=bam_file
-            cur_cmd+=["-S",sam_file]
-            print " ".join(fastqc_cmd)
-            subprocess.check_call(fastqc_cmd)
-            if os.path.exists(bam_file):
-                sys.stderr.write(bam_file+" alignments file already exists. skipping\n")
-            else:
-                print cur_cmd
-                subprocess.check_call(cur_cmd) #call bowtie2
-            if not os.path.exists(bam_file):
-                subprocess.check_call("samtools view -Su "+sam_file+" | samtools sort -o - - > "+bam_file, shell=True)#convert to bam
-                subprocess.check_call("samtools index "+bam_file, shell=True)
-                #subprocess.check_call('samtools view -S -b %s > %s' % (sam_file, bam_file+".tmp"), shell=True)
-                #subprocess.check_call('samtools sort %s %s' % (bam_file+".tmp", bam_file), shell=True)
-            print " ".join(samstat_cmd)
-            subprocess.check_call(samstat_cmd)
+        for condition in condition_dict:
+            rcount=0
+            for r in condition_dict[library]["replicates"]:
+                cur_cleanup=[]
+                rcount+=1
+                target_dir=r["target_dir"]
+                fastqc_cmd=["fastqc","--outdir",target_dir]
+                samstat_cmd=["samstat"]
+                subprocess.call(["mkdir","-p",target_dir])
+                cur_cmd=list(cmd)
+                if "read2" in r:
+                    cur_cmd+=["-1",link_space(r["read1"])," -2",link_space(r["read2"])]
+                    name1=os.path.splitext(os.path.basename(r["read1"]))[0].replace(" ","")
+                    name2=os.path.splitext(os.path.basename(r["read2"]))[0].replace(" ","")
+                    sam_file=os.path.join(target_dir,name1+"_"+name2+".sam")
+                    fastqc_cmd+=[r["read1"],r["read2"]]
+                else:
+                    cur_cmd+=[" -U",link_space(r["read1"])]
+                    name1=os.path.splitext(os.path.basename(r["read1"]))[0].replace(" ","")
+                    sam_file=os.path.join(target_dir,name1+".sam")
+                    fastqc_cmd+=[r["read1"]]
+                cur_cleanup.append(sam_file)
+                bam_file=sam_file[:-4]+".bam"
+                samstat_cmd.append(bam_file)
+                r[genome["genome"]]={}
+                r[genome["genome"]]["bam"]=bam_file
+                cur_cmd+=["-S",sam_file]
+                print " ".join(fastqc_cmd)
+                subprocess.check_call(fastqc_cmd)
+                if os.path.exists(bam_file):
+                    sys.stderr.write(bam_file+" alignments file already exists. skipping\n")
+                else:
+                    print cur_cmd
+                    subprocess.check_call(cur_cmd) #call bowtie2
+                if not os.path.exists(bam_file):
+                    subprocess.check_call("samtools view -Su "+sam_file+" | samtools sort -o - - > "+bam_file, shell=True)#convert to bam
+                    subprocess.check_call("samtools index "+bam_file, shell=True)
+                    #subprocess.check_call('samtools view -S -b %s > %s' % (sam_file, bam_file+".tmp"), shell=True)
+                    #subprocess.check_call('samtools sort %s %s' % (bam_file+".tmp", bam_file), shell=True)
+                print " ".join(samstat_cmd)
+                subprocess.check_call(samstat_cmd)
 
-            for garbage in cur_cleanup:
-                subprocess.call(["rm", garbage])
+                for garbage in cur_cleanup:
+                    subprocess.call(["rm", garbage])
         for garbage in final_cleanup:
             subprocess.call(["rm", garbage])
 
