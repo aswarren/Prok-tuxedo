a611c1557c44f9937052ff6279d1f02852bdae9b
Andrew Warren <aswarren@gmail.com>
2018-11-29 11:29:00 -0500

    adding files from SRA into data structure

--- prok_tuxedo.py
+++ prok_tuxedo.py
@@ -248,6 +248,13 @@ def setup(genome_list, condition_dict, parameters, output_dir, job_data):
                     subprocess.check_call(["p3-sra","--gzip","--out",target_dir,"--metadata-file", meta_file, "--id",srr_id])
                     with open(meta_file) as f:
                         job_meta = json.load(f)
+                    files = job_meta.get("files",[])
+                    for i,f in enumerate(files):
+                        r["read"+str(i+1)]=os.path.join(target_dir, f)
+                    
+
+
+
 
 
 
